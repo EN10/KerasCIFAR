c8ac1f8a2d7ea2a761dc3e4faf519283155be5e0
Enio Fernandes <ef@cic.ac>
2017-12-11 19:40:31 +0000

    save

---
+++ save.py
@@ -0,0 +1,10 @@
+from load_cifar import load_batch
+import numpy as np
+
+images , _ = load_batch()
+images = np.reshape(images, (10000, 3, 32, 32))
+img = np.transpose(images[26], (1, 2, 0))           #   from (3, 32, 32) to  (32, 32, 3)
+
+from PIL import Image
+img = Image.fromarray(img, 'RGB')
+img.save('cat.jpg')
