679cd32473d778483a6934e34bb21589a969fab7
Enio Fernandes <ef@cic.ac>
2017-12-11 12:38:06 +0000

    train test

--- load_cifar.py
+++ load_cifar.py
@@ -1,8 +1,9 @@
-import os
-import cPickle
-import numpy as np
-import random
-random.seed(1) # set a seed so that the results are consistent
+import os               #   for wget and tar
+import cPickle          #   load files
+import numpy as np      #   arrays
+import random           #   to select proportion of false data
+import math             #   absolute value
+random.seed(1)          #   set a seed so that the results are consistent
 
 def download_extract():
     if not (os.path.isdir('cifar-10-batches-py') | os.path.isfile('cifar-10-python.tar.gz')):
@@ -20,53 +21,38 @@ def load_batch():
     f = open(path+file, 'rb')
     dict = cPickle.load(f)
     images = dict['data']
-    #images = np.reshape(images, (10000, 3, 32, 32))
     labels = dict['labels']
-    imagearray = np.array(images)   #   (10000, 3072)
-    labelarray = np.array(labels)   #   (10000,)
+    imagearray = np.array(images)                       #   (10000, 3072)
+    labelarray = np.array(labels)                       #   (10000,)
     
     return imagearray, labelarray
 
-def create_datasets(imagearray, labelarray):
-    train_set_x = np.empty((200,3072))
-    train_set_y = np.empty((1,200),dtype=np.int16)
-
-    i = 0
-    j = 0
-    while (j < 200):                #   200 train images
-        x = random.randint(0,1)
-        if (labelarray[i] == 3):    #   Cats
-            train_set_x[j] = imagearray[i]
-            train_set_y[0,j] = 1    #   Cat is True
-            j+=1
-        elif (x % 2 == 0 and labelarray[i] != 3):   #    NOT Cats
-            train_set_x[j] = imagearray[i]
-            train_set_y[0,j] = 0    # Cat is False
-            j+=1
-        i+=1
-        
-    train_set_x = train_set_x.T     #   Reshape to (3072, 200) 
-    
-    test_set_x = np.empty((50,3072))                #   50 test images
-    test_set_y = np.empty((1,50),dtype=np.int16)
-
-    i = 0
-    j = 0
-    while (j < 50):
-        x = random.randint(0,1)
-        if (labelarray[9999-i] == 3):#  In Reverse Order is Cat
-            test_set_x[j] = imagearray[9999-i]
-            test_set_y[0,j] = 1
-            j+=1
-        elif (x % 2 == 0 and labelarray[i] != 3):
-            test_set_x[j] = imagearray[9999-i]
-            test_set_y[0,j] = 0
-            j+=1
-        i+=1
-
-    test_set_x = test_set_x.T       #   Reshape to (3072, 50)
-
-    train_set_x = train_set_x/255.  #   0-255 -> 0-1
-    test_set_x = test_set_x/255.
+def train_test(imagearray, labelarray, classid):
+    #   200 train images from posistion 0 ++
+    train_set_x, train_set_y    = n_images(imagearray, labelarray, classid, 200, 0)
+    #   50 test images from posistion 9999 --
+    test_set_x, test_set_y      = n_images(imagearray, labelarray, classid, 50, -9999)
 
     return train_set_x, train_set_y, test_set_x, test_set_y
+    
+def n_images(imagearray, labelarray, classid, n, end):
+    set_x = np.empty((n,3072))                          #   Create N images
+    set_y = np.empty((1,n),dtype=np.int16)              #   Create N labels
+
+    i = j = 0
+    while (j < n):                                      #   Select N images
+        x = random.randint(0,1)
+        
+        if (labelarray[math.fabs(end+i)] == classid):   #   Selects Images from Class, end defines start or end
+            set_x[j] = imagearray[math.fabs(end+i)]     #   end selects begining or end of array
+            set_y[0,j] = 1                              #   Set label
+            j+=1
+        elif (x % 2 == 0 and labelarray[i] != classid): #   NOT Images from Class
+            set_x[j] = imagearray[math.fabs(end+i)]
+            set_y[0,j] = 0                              #   Set label
+            j+=1
+        i+=1
+
+    set_x = set_x.T                                     #   Reshape to (3072, N)
+    set_x = set_x/255.                                  #   0-255 -> 0-1
+    return set_x, set_y
