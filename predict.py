f874242640b36e02561892fe6a8e66f90358e017
Enio Fernandes <ef@cic.ac>
2017-12-12 19:08:06 +0000

    pil

--- predict.py
+++ predict.py
@@ -1,13 +1,11 @@
-from tensorflow.python.keras.preprocessing import image
+from PIL import Image
 from tensorflow.python.keras.models import load_model
 import numpy as np
 
-img = image.load_img('cat.jpg', target_size=(32, 32))
-x = image.img_to_array(img)
-x = x.reshape(1,3072)   #   (32, 32, 3) to  (1, 3072)
-x = x.astype(int)
-
-#x.tofile('cat.txt', "\n")
+x = Image.open("cat.jpg")
+x = np.array(x)
+x = x.reshape(1,3072)
+#x.tofile('catpil.txt', "\n")
 
 model = load_model('cifar.h5')
 y = model.predict(x)
