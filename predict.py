ac7b958839d65617ea9b7d5d0b9c93c347e1fc52
Enio Fernandes <ef@cic.ac>
2017-12-12 12:09:37 +0000

    trim

--- predict.py
+++ predict.py
@@ -5,7 +5,8 @@ from tensorflow.python.keras.models import load_model
 x = Image.open("cat.jpg")
 x = np.array(x)
 
-x.tofile('cat.txt',sep="\n")
+x.tofile('cat.txt', "\n")
+
 x = x.reshape(1,3072) / 255
 
 model = load_model('cifar.h5')
