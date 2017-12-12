4e5e00d0030b15427e492f14b7da014eaf7cb269
Enio Fernandes <ef@cic.ac>
2017-12-12 18:49:36 +0000

    space

--- save.py
+++ save.py
@@ -3,7 +3,7 @@ import numpy as np
 
 images , _ = load_batch()
 images = np.reshape(images, (10000, 3, 32, 32))
-img = np.transpose(images[26], (1, 2, 0))           #   from (3, 32, 32) to  (32, 32, 3)
+img = np.transpose(images[26], (1, 2, 0))           #   from (3, 32, 32) to (32, 32, 3)
 
 from PIL import Image
 img = Image.fromarray(img, 'RGB')
