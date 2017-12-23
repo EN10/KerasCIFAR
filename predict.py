770dc494813ec8bd788f843e55d7b619627d35ed
Enio Fernandes <ef@cic.ac>
2017-12-23 18:29:25 +0000

    move

--- predict.py
+++ predict.py
@@ -1,11 +1,9 @@
 from PIL import Image
 import numpy as np
 
-x = Image.open("cat.png")
+x = Image.open("image.png")
 x = np.array(x)
-x = x.reshape(x.shape[0]*x.shape[1], -1).T
-x = x.reshape(1,3072)
-x = x / 255.
+x = np.resize(x,(1, 32, 32, 3))
 
 from tensorflow.python.keras.models import load_model
 model = load_model('keras_cifar10_trained_model.h5')
