2ed7575e3f40a3cabc4ee6f0527fbe955c53a0c3
Enio Fernandes <ef@cic.ac>
2017-12-23 18:03:40 +0000

    arg

--- resize.py
+++ resize.py
@@ -1,5 +1,6 @@
 from scipy.misc import imread, imresize, imsave
+import sys
 
-x = imread("lcat.png")
+x = imread(sys.argv[1])         # filename as arg
 x = imresize(x, (32, 32, 4))
 imsave('cat.png', x)
