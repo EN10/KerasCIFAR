7db181ae1764d1c05a4b68c757046de0cab11ef6
Enio Fernandes <ef@cic.ac>
2017-12-24 00:04:29 +0000

    train

---
+++ train.py
@@ -0,0 +1,19 @@
+from load_cifar import *
+download_extract()
+imagearray, labelarray = load_batch()   #   200 train   50 test     3 is cat id 
+x_train, y_train, x_test, y_test = train_test(imagearray, labelarray, 200, 50, 3)
+
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(units=64, activation='sigmoid', input_shape=(3072,)))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=40, batch_size=64, verbose=1)
+
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+print('Test loss:', loss_and_metrics[0])
+print('Test accuracy:', loss_and_metrics[1])
+
+model.save('sigmoid.h5')
