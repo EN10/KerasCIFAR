876ef8d906dc997077e7aac912de57656ffb80b4
Enio Fernandes <ef@cic.ac>
2018-01-02 09:23:43 +0000

    model

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ x = np.array(x)
 x = np.resize(x,(1, 32, 32, 3))
 
 from tensorflow.python.keras.models import load_model
-model = load_model('keras_cifar10_trained_model.h5')
+model = load_model('model.h5')
 
 labels = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
 y = model.predict(x)
